e3c40e24464b95d69281e3c3496c25d77782db45
Anubhav Aeron <a.aeron.87@gmail.com>
2017-02-26 11:44:55 -0800

    First commit

---
+++ hello.go
@@ -0,0 +1,9 @@
+package hello
+
+import (
+	"fmt"
+)
+
+func SayHi() {
+	fmt.Println("Hi there, how are you? This is Anubhav\n")
+}
---
+++ util/foo.go
@@ -0,0 +1,9 @@
+package util
+
+import (
+	"fmt"
+)
+
+func SayFoo() {
+	fmt.Println("This is foo")
+}
